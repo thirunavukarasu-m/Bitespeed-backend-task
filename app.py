8da584676cad666bc068b4b9c81caa46c7f06c8e
me-thirunavukarasu <mailme.thirunavukarasu@gmail.com>
2025-07-04 22:53:34 +0530

    Added a simple admin login page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from config import Config
 from models import db
 import os
@@ -13,6 +13,18 @@ db.init_app(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == os.getenv("ADMIN_USERNAME") and password == os.getenv("ADMIN_PASSWORD"):
+            pass
+        else:
+            flash('Invalid credentials', 'error')
+    return render_template('login.html')
 
 if __name__ == "__main__":
     with app.app_context():
